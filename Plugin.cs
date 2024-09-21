94c9cf44ac1648c6a719e047739e5be11ff296c9
Snivy Films <120346554+NikkiGardiner1@users.noreply.github.com>
2024-09-20 21:30:51 -0400

    Version Bump

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,7 @@
           public override string Name => "LobbySystem";
           public override string Prefix => Name;
           public override string Author => "@misfiy, updated by Vicious Vikki";
-          public override Version Version => new(1, 1, 0);
+          public override Version Version => new(1, 1, 1);
           public static Plugin Instance { get; private set; } = null!;
 
           public Handler eventHandler { get; set; } = null!;
